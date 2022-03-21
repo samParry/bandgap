93f71352c01a00aee72d596d746770a3ed6bbaed
samParry <u1008557@umail.utah.edu>
2022-03-21 17:42:58 -0600

    Evolving solutions with constants

--- feature_selection/feature_selection.py
+++ feature_selection/feature_selection.py
@@ -16,10 +16,6 @@ from bingo.symbolic_regression.explicit_regression import ExplicitTrainingData
 from bingo.symbolic_regression.explicit_regression import ExplicitRegression
 from bingo.stats.pareto_front import ParetoFront
 
-def agraph_similarity(ag_1, ag_2):
-    """a similarity metric between agraphs"""
-    return ag_1.fitness == ag_2.fitness and ag_1.get_complexity() == ag_2.get_complexity()
-
 # Global parameters
 POP_SIZE = 300
 STACK_SIZE = 10
@@ -27,88 +23,96 @@ MUTATION_PROBABILITY = 0.4
 CROSSOVER_PROBABILITY = 0.4
 MAX_GENS = 1_000
 FIT_TOL = 1e-3
-simplification = True
+use_simplification = True
 regression_metric = 'mse'
 clo_algorithm = 'lm'
 # If I want a scalar fitness -> BFGS
 # If I want a vectorized fitness -> lm (faster)
 
-# Build x/y data
-sheet = pd.read_excel('data.xlsx').to_numpy()
-c1 = sheet[:, 0]    # X_0
-c2 = sheet[:, 1]    # X_1
-h = sheet[:, 2]     # X_2
-r = sheet[:, 3]     # X_3
-phi = sheet[:, 4]   # X_4
-rb = sheet[:, 5]
-X_0 = c1
-X_1 = c2
-X_2 = h
-X_3 = r
-X_4 = phi
-x_data = np.stack((c1, c2, h, r, phi), axis=-1)
-y_data = rb
+def agraph_similarity(ag_1, ag_2):
+    """a similarity metric between agraphs"""
+    return ag_1.fitness == ag_2.fitness and ag_1.get_complexity() == ag_2.get_complexity()
 
-# TODO: Try to normalize data away from being 0s and then invert final equation at the end
+def get_generators(x_data, stack_size: int, use_simplification: bool):
+    """
+    Create and return the agraph, crossover, and mutation generators.
+    :param x_data: numpy array of training data.
+    :param stack_size: Maximum stack size for AGraph.
+    :param use_simplification: Use simplification in AGraphGenerator.
+    :return: agraph_gen, crossover, mutation
+    """
+    component_generator = ComponentGenerator(x_data.shape[1], constant_probability=0.1, num_initial_load_statements=1)
+    component_generator.add_operator("+")
+    component_generator.add_operator("-")
+    component_generator.add_operator("*")
+    component_generator.add_operator("/")
+    agraph_gen = AGraphGenerator(stack_size, component_generator=component_generator,
+                                 use_simplification=use_simplification)
+    crossover = AGraphCrossover()
+    mutation = AGraphMutation(component_generator)
+    return agraph_gen, crossover, mutation
 
-# New superfeature(s)
-# x5 = 2*(phi**4 + 6*phi**3*c1 + 9*phi**4*c1**2)
-# x_5 = x5.reshape(882, 1)
-# x_data = np.append(x_data, x_5, axis=1)
+def get_training_data(spreadsheet: str):
+    """
+    Convert a spreadsheet into x and y features and labels.
+    :param spreadsheet: Path to data spreadsheet.
+    :return: Features and labels in numy arrays (x_data, y_data).
+    """
+    sheet = pd.read_excel(spreadsheet).to_numpy()
+    c1 = sheet[:, 0]  # X_0
+    c2 = sheet[:, 1]  # X_1
+    h = sheet[:, 2]  # X_2
+    r = sheet[:, 3]  # X_3
+    phi = sheet[:, 4]  # X_4
+    y_data = sheet[:, 5]
+    x_data = np.stack((c1, c2, h, r, phi), axis=-1)
+    return x_data, y_data
 
-# x6 = (c1 + phi)**2
-# x_6 = x6.reshape(882, 1)
-# x_data = np.append(x_data, x_6, axis=1)
-#
-# x7 = c1 * phi
-# x_7 = x7.reshape(882, 1)
-# x_data = np.append(x_data, x_7, axis=1)
+def print_pareto_front(pareto_front):
+    """
+    Print the members of the pareto front in sympy format.
+    """
+    print(" FITNESS   COMPLEXITY    EQUATION")
+    for member in pareto_front:
+        eq = member.get_formatted_string("sympy")
+        print("%.3e     " % member.fitness, member.get_complexity(), "     f(X_0) =", eq)
 
-# x8 = 2*x7 + 4*x7*c2**2 - x6
-# x_8 = x8.reshape(882, 1)
-# x_data = np.append(x_data, x_8, axis=1)
-#
-# x9 = x8 - x8 * (phi**2 - c1*phi - c1)**2
-# x_9 = x9.reshape(882, 1)
-# x_data = np.append(x_data, x_9, axis=1)
+def main():
+    # Build x/y data
+    x_data, y_data = get_training_data('data.xlsx')
+    X_0, X_1, X_2, X_3, X_4 = x_data.T
 
-# Agraph generation/variation
-component_generator = ComponentGenerator(x_data.shape[1], constant_probability=0.1, num_initial_load_statements=1)
-component_generator.add_operator("+")
-component_generator.add_operator("-")
-component_generator.add_operator("*")
-component_generator.add_operator("/")
-agraph_gen = AGraphGenerator(STACK_SIZE, component_generator=component_generator, use_simplification=simplification)
-crossover = AGraphCrossover()
-mutation = AGraphMutation(component_generator)
+    # add super feature(s) here
+    X_5 = (12.345913760262821)*(((X_4)*(X_4))*((X_0 + X_4 - ((X_4)*(X_4)))*(X_0 + X_4 - ((X_4)*(X_4)))))
+    x_data = np.append(x_data, X_5.reshape(882, 1), axis=1)
 
-# Explicit evaluation & CLO
-training_data = ExplicitTrainingData(x_data, y_data)
-# TODO: consider writing my own fitness function instead of using ExplicitRegression
-# TODO: Figure out why 'relative' breaks everything. Should normalize fitness of data near zero
-fitness = ExplicitRegression(training_data=training_data, metric=regression_metric, relative=True)
+    X_6 = ((X_1)*(X_1))*(X_3) + X_5 + (X_1)*(X_5) - ((X_1)*(X_3))
+    x_data = np.append(x_data, X_6.reshape(882, 1), axis=1)
 
-# local_opt_fitness = ContinuousLocalOptimization(fitness, algorithm=clo_algorithm)
-# make every constant zero and don't change it (hacky solution)
-local_opt_fitness = ContinuousLocalOptimization(fitness, algorithm=clo_algorithm, param_init_bounds=(0,0), tol=100)
-# local_opt_fitness = ContinuousLocalOptimization(fitness, algorithm='lm', param_init_bounds=(0,0),
-#                                                   options={"ftol": 100, "xtol": 100, "gtol": 100})
-evaluator = Evaluation(local_opt_fitness)
+    X_7 = (1.1437792350379823)*(X_6) + (-2)*(((X_2)*(X_2))*(((X_4)**(-1))*(X_6)))
 
-# Evolution
-ea = AgeFitnessEA(evaluator, agraph_gen, crossover, mutation,
-                  CROSSOVER_PROBABILITY, MUTATION_PROBABILITY, POP_SIZE)
-pareto_front = ParetoFront(secondary_key=lambda ag: ag.get_complexity(),
-                           similarity_function=agraph_similarity)
-t = time()
-island = Island(ea, agraph_gen, POP_SIZE, hall_of_fame=pareto_front)
-island.evolve_until_convergence(max_generations=MAX_GENS, fitness_threshold=FIT_TOL)
-print(f'Elapsed Time: {round((time() - t)/60, 2)}min')
-print("Generation: ", island.generational_age)
-print("Best individual\n f(X_0) =", island.get_best_individual())
+    # Agraph generation/variation
+    agraph_gen, crossover, mutation = get_generators(x_data, STACK_SIZE, use_simplification)
 
-# print results
-print(" FITNESS   COMPLEXITY    EQUATION")
-for member in pareto_front:
-    print("%.3e     " % member.fitness, member.get_complexity(),
-          "     f(X_0) =", member.get_formatted_string("sympy"))
+    # Explicit evaluation & CLO
+    training_data = ExplicitTrainingData(x_data, y_data)
+    fitness = ExplicitRegression(training_data=training_data, metric=regression_metric)
+    local_opt_fitness = ContinuousLocalOptimization(fitness, algorithm=clo_algorithm)
+    # make every constant zero and don't change it (hacky solution)
+    # local_opt_fitness = ContinuousLocalOptimization(fitness, algorithm=clo_algorithm,
+    #                                                   param_init_bounds=(0, 0), tol=100)
+    evaluator = Evaluation(local_opt_fitness)
+
+    # Evolution
+    ea = AgeFitnessEA(evaluator, agraph_gen, crossover, mutation,
+                      CROSSOVER_PROBABILITY, MUTATION_PROBABILITY, POP_SIZE)
+    pareto_front = ParetoFront(secondary_key=lambda ag: ag.get_complexity(),
+                               similarity_function=agraph_similarity)
+    t = time()
+    island = Island(ea, agraph_gen, POP_SIZE, hall_of_fame=pareto_front)
+    island.evolve_until_convergence(max_generations=MAX_GENS, fitness_threshold=FIT_TOL)
+    print(f'Elapsed Time: {round((time() - t) / 60, 2)}min')
+    print_pareto_front(pareto_front)
+
+if __name__ == '__main__':
+    main()
