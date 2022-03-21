a49d02e92105d97e1cd949b33787bb7eacab7c87
samParry <u1008557@umail.utah.edu>
2022-03-21 16:43:48 -0600

    Added example to remind myself how to use function

--- feature_selection/true_complexity.py
+++ feature_selection/true_complexity.py
@@ -16,3 +16,12 @@ def get_complexity(eq: str, *nested_eqs):
         eq = eq.replace(x, nested_eqs[i])
     graph = AGraph(sympy_representation=eq)
     return graph.get_complexity()
+
+def main():
+    """Example of how to use get_complexity"""
+    X_5 = '(2)*(((X_4)*(X_4))*(((3)*(X_0) + X_4)*((3)*(X_0) + X_4)))'
+    X_6 = 'X_5 + (X_0)*((3)*(X_5) + (-2)*((X_5)*(X_5)))'
+    print(get_complexity(X_6, X_5))
+
+if __name__ == '__main__':
+    main()
