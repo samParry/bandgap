25b40125a106ef3b8011692835c0ce39f56a6ee2
samParry <u1008557@umail.utah.edu>
2022-03-21 15:45:06 -0600

    Need to implement data normalization

---
+++ feature_selection/normalize_data.py
@@ -0,0 +1,11 @@
+""" @author: Sam Parry u1008557 """
+
+import numpy as np
+
+# TODO: find a way to normalize data so it's not all zeros
+
+def normalize_y(y):
+    return
+
+def denormalize_y(y):
+    return
