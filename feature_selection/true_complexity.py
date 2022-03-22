0d9723216224220e10b1fd3042a7da5fd643c113
samParry <u1008557@umail.utah.edu>
2022-03-22 14:34:17 -0600

    Encountered an error in Davids string parsing code

--- feature_selection/true_complexity.py
+++ feature_selection/true_complexity.py
@@ -19,12 +19,18 @@ def get_complexity(eq: str, *nested_eqs):
     return graph.get_complexity()
 
 def main():
-    """Example of how to use get_complexity"""
     x5 = '(12.345913760262821)*(((X_4)*(X_4))*((X_0 + X_4 - ((X_4)*(X_4)))*(X_0 + X_4 - ((X_4)*(X_4)))))'
     print(AGraph(sympy_representation=x5).get_complexity())
     x6 = '((X_1)*(X_1))*(X_3) + X_5 + (X_1)*(X_5) - ((X_1)*(X_3))'.replace('X_5', x5)
     print(AGraph(sympy_representation=x6).get_complexity())
     x7 = '(1.1437792350379823)*(X_6) + (-2)*(((X_2)*(X_2))*(((X_4)**(-1))*(X_6)))'.replace('X_6', x6)
+    print(AGraph(sympy_representation=x7).get_complexity())
+
+    x5 = '0.010768645307381816 + (0.08375951370152104)*((X_1 + (-11.27548093458503)*((0.2734952789489305 + X_0)*(X_4 - ((X_2)*(X_2)))))*(X_1 + (-11.27548093458503)*((0.2734952789489305 + X_0)*(X_4 - ((X_2)*(X_2))))))'
+    print(AGraph(sympy_representation=x5).get_complexity())
+    x6 = '(8.983347101758822)*((X_1)*((X_5)*(-0.05372342969980863 + X_1 + X_5 - (X_2)))) + (1.5139595910990273)*(-0.0031797777791105816 + (X_5)*(0.2806226824618265 + X_5))'.replace('X_5', x5)
+    print(AGraph(sympy_representation=x6).get_complexity())
+    x7 = '(1.4683663251419399 + ((-1)*(0.054264452711012254))*(((X_2)**(-1))*(X_4)) + (20.761811418218716)*((X_0)*((0.057097261607712654 + X_2)*(-0.46498757312848854 + (0.054264452711012254)*(((X_2)**(-1))*(X_4))))))*(X_6)'.replace('X_6', x6)
     print(AGraph(sympy_representation=x7).get_complexity())
 
 if __name__ == '__main__':
