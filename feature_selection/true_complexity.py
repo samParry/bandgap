50d85361d0736c769cac1c8fe48e4975be67d681
samParry <u1008557@umail.utah.edu>
2022-03-24 14:30:17 -0600

    Deleted unused function

--- feature_selection/true_complexity.py
+++ feature_selection/true_complexity.py
@@ -2,31 +2,10 @@
 
 from bingo.symbolic_regression import AGraph
 
-# TODO: Not working for 2 inputs on nested_eqs
-def get_complexity(eq: str, *nested_eqs):
-    """
-    Returns the true complexity of an equation containing multiple nested
-    equations in the form of other variables.
-    :param eq: A sympy representation of an equation.
-    :param nested_eqs: Equation string for all nested equations represented
-        as variables in `eq`.
-    :return: The true complexity.
-    """
-    for i in range(len(nested_eqs)):
-        x = f'X_{5+i}'
-        eq = eq.replace(x, nested_eqs[i])
-    graph = AGraph(sympy_representation=eq)
-    return graph.get_complexity()
-
 def main():
-    x5 = '(12.345913760262821)*(((X_4)*(X_4))*((X_0 + X_4 - ((X_4)*(X_4)))*(X_0 + X_4 - ((X_4)*(X_4)))))'
-    print(AGraph(sympy_representation=x5).get_complexity())
-    x6 = '((X_1)*(X_1))*(X_3) + X_5 + (X_1)*(X_5) - ((X_1)*(X_3))'.replace('X_5', x5)
-    print(AGraph(sympy_representation=x6).get_complexity())
-    x7 = '(1.1437792350379823)*(X_6) + (-2)*(((X_2)*(X_2))*(((X_4)**(-1))*(X_6)))'.replace('X_6', x6)
-    print(AGraph(sympy_representation=x7).get_complexity())
 
-    x5 = '0.010768645307381816 + (0.08375951370152104)*((X_1 + (-11.27548093458503)*((0.2734952789489305 + X_0)*(X_4 - ((X_2)*(X_2)))))*(X_1 + (-11.27548093458503)*((0.2734952789489305 + X_0)*(X_4 - ((X_2)*(X_2))))))'
+    # relative data
+    x5 = '2.2756191691692638 + (-0.3272193764716462 + (-2.361493765317003)*(X_2 - ((X_0)*(X_4))))*(2.4443625610734605 + X_1 + (-2.361493765317003)*(X_2 - ((X_0)*(X_4))) + (-2.361493765317003)*((X_2 - ((X_0)*(X_4)))*(1.4784424822276252 + X_1 + (-2.361493765317003)*(X_2 - ((X_0)*(X_4))))))'
     print(AGraph(sympy_representation=x5).get_complexity())
     x6 = '(8.983347101758822)*((X_1)*((X_5)*(-0.05372342969980863 + X_1 + X_5 - (X_2)))) + (1.5139595910990273)*(-0.0031797777791105816 + (X_5)*(0.2806226824618265 + X_5))'.replace('X_5', x5)
     print(AGraph(sympy_representation=x6).get_complexity())
